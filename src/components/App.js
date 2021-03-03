0e5ffe5d3b1e49b8fa5caa4651367911f6a5c208
Carmen Ramos <carmen.rama91@gmail.com>
2021-03-03 16:40:25 +0100

    add first commit incluide clone repo and start it

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../stylesheets/App.scss";
+import React, { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rick and Morty</h1>
+    </div>
+  );
+}
+
+export default App;
