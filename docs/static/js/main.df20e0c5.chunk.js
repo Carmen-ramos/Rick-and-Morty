e6de3613b28fc301356d0a74aa8b3f04cf60259d
Carmen Ramos <carmen.rama91@gmail.com>
2021-03-07 04:07:24 +0100

    add run docs

--- docs/static/js/main.df20e0c5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-react-project"]=this["webpackJsonpmy-react-project"]||[]).push([[0],{11:function(t,e,c){"use strict";c.r(e);c(1);var n=c(3),r=c.n(n),o=(c(8),c(9),c(0));var s=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"Rick and Morty"})})};r.a.render(Object(o.jsx)(s,{}),document.getElementById("root"))},8:function(t,e,c){},9:function(t,e,c){}},[[11,1,2]]]);
-//# sourceMappingURL=main.df20e0c5.chunk.js.map
